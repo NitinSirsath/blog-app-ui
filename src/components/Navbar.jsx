57f264a066fb72c86a133cc73b67740bfac930db
lostNIII3 <73011138+lostNIII3@users.noreply.github.com>
2022-02-16 18:31:41 +0530

    login added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,57 +1,31 @@
 import React from "react";
 import "./navbar.css";
+import Modal from "react-modal";
+import { useState } from "react";
+import FormModal from "./FormModal";
 // import styled from 'styled-components'
 
 const Navbar = () => {
+  const [modal, setModal] = useState(false);
+
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+  };
+
   return (
     <>
-      {/* <div  class="d-flex justify-content-between">
-    <div className='p-4'><span className='text-success'>ATG</span>.WORLD</div>
-    <div>
-
-<div class="input-group pb-4 pt-4">
-  <input type="text" class="form-control w-100 rounded-pill" placeholder="Search your favourite group in ATG" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
-</div>
-
-    </div>
-    <div className='p-4'><button >signup</button></div>
-    </div> */}
       <div className="nav-bar vw-100">
         <div className="p-3 ps-5">
           <span className="text-success">ATG</span>.WORLD
         </div>
 
-        {/* <div class="input-group p-2 ">
-          <input type="text" className="form-control w-30  rounded-pill" placeholder="Search your favourite group in ATG" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
-        </div> */}
-
-        {/* <div className="input-group rounded-pill">
-          <div className="input-group-prepend">
-            <span className="input-group-text" id="basic-addon1">
-              &
-            </span>
-          </div>
-          <input
-            type="text"
-            className="form-control "
-            placeholder="Username"
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-          />
-        </div> */}
-        {/* <div className="input-group">
-          <span className="input-group-prepend">
-            <button className="btn rounded-pill border-0 ml-5" type="button">
-              
-            </button>
-          </span>
-          <input
-            className="form-control py-2 rounded-pill mr-n1 pr-n5"
-            placeholder="Search your favourite group in ATG"
-            type="search"
-            value=""
-          />
-        </div> */}
         <div className="form-group  has-search">
           <span className=" form-control-feedback">
             <img
@@ -67,9 +41,16 @@ const Navbar = () => {
           />
         </div>
         <p className="m-0 pe-4 grid-item p-0">
-          Create account.<span className="text-primary">It's free</span>
+          Create account.
+          <span onClick={() => setModal(true)} className="text-primary signing">
+            It's free
+          </span>
           <span> &#8595;</span>
         </p>
+
+        <Modal style={customStyles} isOpen={modal}>
+          <FormModal className="modal" setModal={setModal} />
+        </Modal>
       </div>
     </>
   );
